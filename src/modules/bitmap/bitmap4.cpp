0902f85a9d93c12034fb28cfe0997371bb4e2780
aojwang <aojw2008@gmail.com>
2012-11-16 18:51:32 +0800

    refine comments

--- src/modules/bitmap/bitmap4.cpp
+++ src/modules/bitmap/bitmap4.cpp
@@ -33,34 +33,55 @@ bitmap4_and::run(AnyType &args){
     return BitmapUtil::bitmap_and<int32>(args);
 }
 
+
+/**
+ * @brief the operator "OR" implementation
+ */
 AnyType
 bitmap4_or::run(AnyType &args){
     return BitmapUtil::bitmap_or<int32>(args);
 }
 
+
+/**
+ * @brief get the number of bits whose value is 1.
+ */
 AnyType
 bitmap4_nonzero_count::run(AnyType &args){
     return BitmapUtil::bitmap_nonzero_count<int32>(args);
 }
 
+
+/**
+ * @brief get the positions of the non-zero bits.
+ */
 AnyType
 bitmap4_nonzero_positions::run(AnyType &args){
     return BitmapUtil::bitmap_nonzero_positions<int32>(args);
 }
 
 
+/**
+ * @brief the in function for the bitmap data type
+ */
 AnyType
 bitmap4_in::run(AnyType &args){
     return BitmapUtil::bitmap_in<int32>(args);
 }
 
 
+/**
+ * @brief the out function for the bitmap data type
+ */
 AnyType
 bitmap4_out::run(AnyType &args){
     return BitmapUtil::bitmap_out<int32>(args);
 }
 
 
+/**
+ * @brief get an integer array from the bitmap.
+ */
 AnyType
 bitmap4_return_array::run(AnyType &args){
     return AnyType(args[0].getAs<ArrayHandle<int32> >(true),
--- src/modules/bitmap/bitmap8.cpp
+++ src/modules/bitmap/bitmap8.cpp
@@ -17,6 +17,7 @@ bitmap8_agg_sfunc::run(AnyType &args){
    return BitmapUtil::bitmap_agg_sfunc<int64>(args);
 }
 
+
 /**
  * @brief the pre-function for the bitmap aggregate.
  */
@@ -34,34 +35,55 @@ bitmap8_and::run(AnyType &args){
     return BitmapUtil::bitmap_and<int64>(args);
 }
 
+
+/**
+ * @brief the operator "OR" implementation
+ */
 AnyType
 bitmap8_or::run(AnyType &args){
     return BitmapUtil::bitmap_or<int64>(args);
 }
 
+
+/**
+ * @brief get the number of bits whose value is 1.
+ */
 AnyType
 bitmap8_nonzero_count::run(AnyType &args){
     return BitmapUtil::bitmap_nonzero_count<int64>(args);
 }
 
+
+/**
+ * @brief get the positions of the non-zero bits.
+ */
 AnyType
 bitmap8_nonzero_positions::run(AnyType &args){
     return BitmapUtil::bitmap_nonzero_positions<int64>(args);
 }
 
 
+/**
+ * @brief the in function for the bitmap data type
+ */
 AnyType
 bitmap8_in::run(AnyType &args){
     return BitmapUtil::bitmap_in<int64>(args);
 }
 
 
+/**
+ * @brief the out function for the bitmap data type
+ */
 AnyType
 bitmap8_out::run(AnyType &args){
     return BitmapUtil::bitmap_out<int64>(args);
 }
 
 
+/**
+ * @brief get an integer array from the bitmap.
+ */
 AnyType
 bitmap8_return_array::run(AnyType &args){
     return AnyType(args[0].getAs<ArrayHandle<int64> >(true),
