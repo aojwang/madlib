397969e478167ec8c75c06953d72a927d40ac512
aojwang <aojw2008@gmail.com>
2012-12-07 15:44:57 +0800

    Compressed Bitmap: build a compressed bitmap for Apriori and other modules Jira: MADLIB-767
    We will create a UDT for the bitmap, and provide some functions for that.
    1) operators: |, &, =, <, <=, >, >=, <>
    2) casts: bitmap to varbit/array, and int4/int8 array to bitmap
    3) functions: bitmap_agg, bitmap_nonzero_positions and bitmap_nonzero_count

---
+++ src/modules/bitmap/Bitmap.hpp
@@ -0,0 +1,39 @@
+/*
+ * Bitmap.hpp
+ *
+ * This file contains the interfaces for get bitmap from arguments and
+ * return the bitmap as AnyType
+ */
+
+#ifndef MADLIB_MODULES_BITMAP_BITMAP_HPP
+#define MADLIB_MODULES_BITMAP_BITMAP_HPP
+
+// the public interfaces for bitmap
+#define RETURN_BITMAP_INTERNAL(val, T) \
+            return AnyType(ArrayHandle<T>(val), InvalidOid)
+#define RETURN_BITMAP_NULL_INTERNAL(val, T) \
+            const ArrayType* res = val; \
+            return NULL != res ? AnyType(ArrayHandle<T>(res), InvalidOid) : AnyType()
+#define RETURN_ARRAY(val, T) \
+            return AnyType(ArrayHandle<T>(val))
+#define RETURN_BASE(val) \
+            return AnyType(val, (bool)false)
+#define GETARG_MUTABLE_BITMAP_INTERNAL(arg, T) \
+            ((arg).getAs< MutableArrayHandle<T> >(false, false))
+#define GETARG_CLONED_BITMAP_INTERNAL(arg, T) \
+            ((arg).getAs< MutableArrayHandle<T> >(false, true))
+#define GETARG_IMMUTABLE_BITMAP_INTERNAL(arg, T) \
+            ((arg).getAs< ArrayHandle<T> >(false, false))
+
+// return
+#define RETURN_BITMAP(val)             RETURN_BITMAP_INTERNAL(val, int32_t)
+#define RETURN_BITMAP_NULL(val)        RETURN_BITMAP_NULL_INTERNAL(val, int32_t)
+#define RETURN_INT4_ARRAY(val)         RETURN_ARRAY(val, int32_t)
+#define RETURN_INT8_ARRAY(val)         RETURN_ARRAY(val, int64_t)
+
+// get arguments
+#define GETARG_MUTABLE_BITMAP(arg)     GETARG_MUTABLE_BITMAP_INTERNAL(arg, int32_t)
+#define GETARG_CLONED_BITMAP(arg)      GETARG_CLONED_BITMAP_INTERNAL(arg, int32_t)
+#define GETARG_IMMUTABLE_BITMAP(arg)   GETARG_IMMUTABLE_BITMAP_INTERNAL(arg, int32_t)
+
+#endif /* MADLIB_MODULES_BITMAP_BITMAP_HPP */
